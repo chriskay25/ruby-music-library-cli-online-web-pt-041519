e51a0f8f9371c3087984cea6655ae9820f837d98
Chris Kay <chriskay25@gmail.com>
2019-06-15 15:58:32 +0000

    Automatically backed up by Learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,75 @@
+class MusicLibraryController
+  
+  attr_accessor :music_importer
+  
+  def initialize(path = "./db/mp3s") 
+    @music = MusicImporter.new(path).import
+      # returns array of ["artist - song - genre.mp3", "artist - song...."]
+    @music.each {|file| file.chomp!(".mp3")}
+      #removes the .mp3 from the genres to make the array easier to work with
+  end 
+  
+  def call 
+    user_input = nil
+    until user_input == "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      user_input = gets
+    end 
+  end 
+  
+  def list_songs 
+    # count = 1 
+    # importer_alphabetized = @music.sort {|a, b| a.name <=> b.name}
+    # @music.each do |song| 
+    #   puts "#{count}. #{song}"
+    #   count += 1 
+    # end 
+    songs = Song.all.collect {|song| song}
+    binding.pry 
+    songs.sort {|a, b| a.name <=> b.name}
+  end 
+
+  
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
