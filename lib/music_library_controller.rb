31fe667cedb951119e74eb66e52ba4c6cb73bdde
Chris Kay <chriskay25@gmail.com>
2019-06-15 17:40:12 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -32,7 +32,7 @@ class MusicLibraryController
   # end 
   
   def list_songs 
-    self.song_list = Song.all.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |song, index| 
+    @song_list = Song.all.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |song, index| 
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end 
   end 
